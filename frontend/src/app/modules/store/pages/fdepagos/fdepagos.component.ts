a59111d2f59c4526d1e8d1fdaeaac2593487cb08
villarrealtay <villarrealtay@gmail.com>
2023-07-02 19:30:57 -0300

    carrito

--- frontend/src/app/modules/store/pages/fdepagos/fdepagos.component.ts
+++ frontend/src/app/modules/store/pages/fdepagos/fdepagos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 declare var paypal;
 
@@ -7,41 +8,51 @@ declare var paypal;
   templateUrl: './fdepagos.component.html',
   styleUrls: ['./fdepagos.component.css']
 })
-export class FdepagosComponent implements OnInit{
-  @ViewChild('paypal', {static:true}) paypalElement : ElementRef;
+export class FdepagosComponent implements OnInit {
+  @ViewChild('paypal', {static:true}) paypalElement: ElementRef;
+  producto: any;
 
-  producto = {
-    description: "Taller de Musica",
-    precio: 250,
-    img: "Img"
-  }
+  constructor(private http: HttpClient) { }
 
-
-  ngOnInit(){
+  ngOnInit() {
+    this.obtenerDetallesProducto(); 
 
     paypal
-    .Buttons({
-      createOrder:(data, actions) => {
-        return actions.order.create({
-          purchase_units:[
-            {
-              description: this.producto.description,
-              amount : {
-
-                value: this.producto.precio
+      .Buttons({
+        createOrder: (data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                description: this.producto.description,
+                amount: {
+                  value: this.producto.precio
+                }
               }
-            }
-          ]
-        })
-      },
-      onApprove: async (data, action)=>{
-        const order = await action.order.capture();
-        console.log(order);
-      },
-      onError: err=>{
-        console.log(err);
-      }
-    })
-    .render(this.paypalElement.nativeElement);
+            ]
+          });
+        },
+        onApprove: async (data, action) => {
+          const order = await action.order.capture();
+          console.log(order);
+        },
+        onError: err => {
+          console.log(err);
+        }
+      })
+      .render(this.paypalElement.nativeElement);
+  }
+
+  obtenerDetallesProducto() {
+    const url = 'http://localhost:8000/api/v1/payment/checkout'; 
+
+    this.http.get(url)
+      .subscribe(
+        (response: any) => {
+          this.producto = response;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 }
